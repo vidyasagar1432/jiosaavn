6651d381f326eef0a0710302ea5768652b8e18cc
vidya sagar <svidya051@gmail.com>
2021-11-27 12:49:20 +0530

    Added github url

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(name='JioSaavn',
-        version='0.1',
-        description='JioSaavn Api',
-        url='#',
-        author='vidya sagar',
-        install_requires=['aiohttp'],
-        author_email='',
-        packages=setuptools.find_packages(),
-        zip_safe=False)
+version='v0.0.2-alpha',
+description='JioSaavn Api',
+url='https://github.com/vidyasagar1432/JioSaavn',
+author='vidya sagar',
+install_requires=['aiohttp'],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
